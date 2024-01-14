88ab91e1189ae16ec8c4bae6779604af7f341ffe
Scion-Kin <dinturner17@gmail.com>
2024-01-14 07:34:03 +0200

    changes

--- console.py
+++ console.py
@@ -1,20 +1,54 @@
 #!/usr/bin/python3
 ''' A custom commnand-line prompt handler '''
 import cmd
-
+import sys
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
+
+    classes = {
+               'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
+              }
+
+    prompt = "(hbnb) "
+
     def do_EOF(self, line):
         '''Exits the program'''
         return True
 
     def do_quit(self, line):
-        '''Quit command to exit the program'''
         return True
 
-    def help_(self):
-        ''' Helps with available shell command'''
-        help()
+    def help_quit(self):
+        print('Quit command to exit the program\n')
+
+    def emptyline(self):
+        pass
+
+    def do_create(self, args):
+        ''' Creates a new BaseModel instance and saves it to a file'''
+        if args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        elif not args:
+            print("** class name missing **")
+            return
+
+        else:
+            new = HBNBCommand.classes[args]()
+            storage.save()
+            print(new.id)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
