53fa6d64e50d3dceb58f9b589c72ac55dd297e9c
Scion-Kin <dinturner17@gmail.com>
2024-01-14 13:05:35 +0200

    tasks completed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,31 +5,34 @@ import json
 
 class FileStorage:
     ''' Class definition for file storage handling '''
+
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         ''' returns class info '''
+
         return FileStorage.__objects
 
     def new(self, obj):
         ''' stores the info of a new instance '''
+
         name = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[name] = obj
-        return self.__objects
 
     def save(self):
         ''' saves info of a new instance in a file '''
+
         with open(FileStorage.__file_path, 'w') as f:
             r_dict = {}
             r_dict.update(FileStorage.__objects)
             for key, value in r_dict.items():
-                r_dict[key] = value if isinstance(value, dict)\
-                    else value.to_dict()
+                r_dict[key] = value.to_dict()
             json.dump(r_dict, f)
 
     def reload(self):
         ''' loads info from a json file '''
+
         from models.base_model import BaseModel
         from models.amenity import Amenity
         from models.city import City
